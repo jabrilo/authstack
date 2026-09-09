626a75a081fbb079ba150b14b43680ef229093ff
Jacques Briand <57547769+jabrilo@users.noreply.github.com>
2026-09-08 23:54:21 -0400

    refactor(authstack): decouple AuthStack from auth impl.
    Move the session manager from config to AuthStack. Remove the config
    struct and authentication configuration fields from AuthStack.
    Also refactor New to acept a session manager that implements
    SessionManager, remove the Option type and associated
    configuration function (WithXYZ -> Option).

--- authstack.go
+++ authstack.go
@@ -7,70 +7,15 @@ import (
 	"fmt"
 )
 
-type config struct {
-	sessionManager SessionManager
-	password       PasswordConfig
-	oidc           OIDCConfig
-	staticToken    StaticTokenConfig
-	stateGenFn     func() (string, error)
-}
-
 type AuthStack struct {
-	cfg config
+	sessions SessionManager
 }
 
-type Option func(*config) error
-
-func New(opts ...Option) (*AuthStack, error) {
-	var cfg config
-
-	cfg.stateGenFn = defaultStateGenerator
-
-	for _, opt := range opts {
-		if err := opt(&cfg); err != nil {
-			return nil, fmt.Errorf("authstack: option failed: %w", err)
-		}
+func New(sm SessionManager) (*AuthStack, error) {
+	if sm == nil {
+		return nil, errors.New("authstack: session manager is required")
 	}
-
-	return &AuthStack{cfg: cfg}, nil
-}
-
-func WithSessionManager(sm SessionManager) Option {
-	return func(c *config) error {
-		c.sessionManager = sm
-		return nil
-	}
-}
-
-func WithProviderPassword(cfg PasswordConfig) Option {
-	return func(c *config) error {
-		c.password = cfg
-		return nil
-	}
-}
-
-func WithProviderOIDC(cfg OIDCConfig) Option {
-	return func(c *config) error {
-		c.oidc = cfg
-		return nil
-	}
-}
-
-func WithProviderStaticToken(cfg StaticTokenConfig) Option {
-	return func(c *config) error {
-		c.staticToken = cfg
-		return nil
-	}
-}
-
-func WithStateGenerator(fn func() (string, error)) Option {
-	return func(c *config) error {
-		if fn == nil {
-			return errors.New("authstack: state generator function cannot be nil")
-		}
-		c.stateGenFn = fn
-		return nil
-	}
+	return &AuthStack{sessions: sm}, nil
 }
 
 func defaultStateGenerator() (string, error) {
